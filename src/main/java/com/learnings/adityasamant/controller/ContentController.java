bfea88948ffed9fa68acfa82dfe45366e28e3c90
Aditya Samant <adityasamant.learnings@gmail.com>
2023-05-26 19:42:38 +0530

    Basic rest endpoints added.

---
+++ src/main/java/com/learnings/adityasamant/controller/ContentController.java
@@ -0,0 +1,33 @@
+package com.learnings.adityasamant.controller;
+
+import com.learnings.adityasamant.model.Content;
+import com.learnings.adityasamant.repository.ContentCollectionRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/content")
+public class ContentController {
+
+    private final ContentCollectionRepository repository;
+
+    public ContentController(ContentCollectionRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("")
+    public List<Content> findAll() {
+        return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Content findById(@PathVariable Integer id) {
+        return repository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Content not found!"));
+    }
+}
--- src/main/java/com/learnings/adityasamant/repository/ContentCollectionRepository.java
+++ src/main/java/com/learnings/adityasamant/repository/ContentCollectionRepository.java
@@ -1,10 +1,15 @@
 package com.learnings.adityasamant.repository;
 
 import com.learnings.adityasamant.model.Content;
+import com.learnings.adityasamant.model.Status;
+import com.learnings.adityasamant.model.Type;
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class ContentCollectionRepository {
@@ -17,5 +22,16 @@ public class ContentCollectionRepository {
 
     public List<Content> findAll() {
         return contents;
+    }
+
+    public Optional<Content> findById(Integer id) {
+        return contents.stream().filter(c -> c.id().equals(id)).findFirst();
+    }
+
+    @PostConstruct
+    private void init(){
+        Content content = new Content(1, "My First Blog Post", "My First Blog Post",
+                Status.IDEA, Type.ARTICLE, LocalDateTime.now(), null, "");
+        contents.add(content);
     }
 }
