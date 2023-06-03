92dc1fa256cc5dc2ae5a934f7117c672e83a0eaa
Aditya Samant <adityasamant.learnings@gmail.com>
2023-06-03 07:27:52 +0530

    Used ConfigurationProperties

--- src/main/java/com/learnings/adityasamant/Application.java
+++ src/main/java/com/learnings/adityasamant/Application.java
@@ -1,5 +1,6 @@
 package com.learnings.adityasamant;
 
+import com.learnings.adityasamant.config.ContentCalendarProperties;
 import com.learnings.adityasamant.model.Content;
 import com.learnings.adityasamant.model.Status;
 import com.learnings.adityasamant.model.Type;
@@ -7,10 +8,12 @@ import com.learnings.adityasamant.repository.ContentRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
 import java.time.LocalDateTime;
 
+@EnableConfigurationProperties(ContentCalendarProperties.class)
 @SpringBootApplication
 public class Application {
 
